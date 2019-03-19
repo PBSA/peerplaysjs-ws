4c3f06a4e3d627af790ff340c2fc59ab1c11a7a9
Adrian <a.metzler@pbsa.info>
2019-03-19 11:32:40 -0300

    built libs

--- dist/src/ConnectionManager.js
+++ dist/src/ConnectionManager.js
@@ -37,9 +37,7 @@ var Manager = function () {
   };
 
   Manager.prototype.isURL = function isURL(str) {
-    var endpointPattern = new RegExp('((^(?:ws(s)?:\\\/\\\/)|(?:http(s)?:\\\/\\\/))+((?:[^\\\/\\\/\\\.])+\??(?:[-\\+=&;%@.\\w_]*)((#?(?:[\\w])*)(:?[0-9]*))))');
-
-    //((^(?:ws(s)?:\\\/\\\/)|(?:http(s)?:\\\/\\\/))+((?:[^\\\/\\\/\\\.])+\??(?:[-\\+=&;%@.\\w_]*)((#?(?:[\\w])*)(:?[0-9]*))))
+    var endpointPattern = new RegExp('((^(?:ws(s)?:\\/\\/)|(?:http(s)?:\\/\\/))+((?:[^\\/\\/\\.])+\\??(?:[-\\+=&;%@.\\w_]*)((#?(?:[\\w])*)(:?[0-9]*))))');
 
     return endpointPattern.test(str);
   };
